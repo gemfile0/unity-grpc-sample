bf33a34cf3e345694e0bbc3d8dabb7d0dbc4e98f
gemfile0 <gemfile0@gmail.com>
2024-07-03 12:24:13 +0900

    YetAnotherHttpHandler 사용 예제, 서버 응답 확인함

--- Assets/UnityGrpcSample/Scripts/SampleScene.cs
+++ Assets/UnityGrpcSample/Scripts/SampleScene.cs
@@ -1,8 +1,23 @@
+using Cysharp.Net.Http;
+using Grpc.Net.Client;
+using GrpcService1;
+using TMPro;
 using UnityEngine;
+using static GrpcService1.Greeter;
 
 public class SampleScene : MonoBehaviour
 {
+    [SerializeField] private string _uri = "http://localhost:5288";
     [SerializeField] private string _name;
+    [SerializeField] private TextMeshProUGUI _responseText;
 
+    private async void Start()
+    {
+        using var handler = new YetAnotherHttpHandler() { Http2Only = true };
+        using var channel = GrpcChannel.ForAddress(_uri, new GrpcChannelOptions() { HttpHandler = handler });
+        var greeter = new GreeterClient(channel);
 
+        var result = await greeter.SayHelloAsync(new HelloRequest { Name = _name });
+        _responseText.text = result.Message;
+    }
 }
